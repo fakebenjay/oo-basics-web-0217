1185bbf9108966c3e242e819f2453e7f3a3a6055
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-08 08:49:59 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    return @title
+  end
 
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    return @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    return @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    return @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
